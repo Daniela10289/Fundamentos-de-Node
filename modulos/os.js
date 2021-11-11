6b6ceb4ad0c2514f1254ad8fa4cb943969eb6cc2
Daniela10289 <dpalacio2200@gmail.com>
2021-11-11 15:17:38 -0500

    modulo de os sistema operativo

---
+++ modulos/os.js
@@ -0,0 +1,38 @@
+const os = require('os');
+
+// // dice que sistema operativo tengo: x64
+// console.log(os.arch());
+
+// // dice que plataforma estoy usando
+// console.log(os.platform());
+
+// // dice la caracteristicas del sistema
+// console.log(os.cpus());
+
+// //errores y señales del sistema
+// console.log(os.constants);
+
+// // info sobre la memoria del sistema
+// console.log(os.freemem);
+
+// //acceder al espacio en memoria
+// const SIZE = 1024;
+// function kb(bytes) { return bytes / SIZE }
+// function mb(bytes) { return kb(bytes) / SIZE }
+// function gb(bytes) { return mb(bytes) / SIZE }
+
+// console.log(os.freemem());
+// console.log(kb(os.freemem()));
+// console.log(mb(os.freemem()));
+// console.log(gb(os.freemem()));
+
+// // memoria total
+// console.log(gb(os.totalmem()));
+
+
+console.log(os.homedir());
+console.log(os.tmpdir());
+console.log(os.hostname());
+console.log(os.networkInterfaces());
+
+
