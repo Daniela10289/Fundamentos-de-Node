e7cdd548f4a58ea856f1c021cd58a5c9f1ada5c0
Daniela10289 <dpalacio2200@gmail.com>
2021-11-16 14:51:20 -0500

    Manejo de Buffers

---
+++ memoria/buffer.js
@@ -0,0 +1,15 @@
+// guardar espacios en memoria
+// let buffer = Buffer.from([1, 2, 3]);
+
+let buffer = Buffer.from('Hola');
+// console.log(buffer.toString());
+
+// mostrar el abecedario 
+let abc = Buffer.alloc(26);
+console.log(abc);
+
+for (let i = 0; i < 26; i++) {
+    abc[i] = i + 97;
+}
+
+console.log(abc.toString());
